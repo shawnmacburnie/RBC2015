89e1a63c281cbacfbc44c5f1dcc2d4dd9c566b33
Chris Malloy <cjmalloy@gmail.com>
2016-09-12 17:05:42 -0300

    Renamed folders.

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const http = HTTP.Server(app);
 const io = SocketIO(http);
 
 app.use(bodyParser.json());
-app.use(express.static('target'));
+app.use(express.static('www'));
 
 //Defines a route at localhost:3000/
 app.get('/', function(req,res) {
-    res.sendFile(__dirname + '/target/index.html');
+    res.sendFile(__dirname + '/www/index.html');
 });
 
 app.get('/api/snapshot', function(req,res) {
